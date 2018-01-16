702bc5abbde9413fcf832af6ff80b0788c21ab03
Vinay Shenoy <vinaysshenoy@gmail.com>
2018-01-16 16:39:56 +0530

    Added OkLogNetDb to OkLogNet

--- lib/src/main/kotlin/com/vinaysshenoy/oklognet/OkLogNet.kt
+++ lib/src/main/kotlin/com/vinaysshenoy/oklognet/OkLogNet.kt
@@ -1,5 +1,8 @@
 package com.vinaysshenoy.oklognet
 
+import android.arch.persistence.room.Room
+import android.content.Context
+import com.vinaysshenoy.oklognet.db.OkLogNetDb
 import okhttp3.Interceptor
 import okhttp3.Interceptor.Chain
 import okhttp3.Response
@@ -7,7 +10,13 @@ import okhttp3.Response
 /**
  * Created by vinaysshenoy on 16/01/18.
  */
-class OkLogNet: Interceptor {
+class OkLogNet(context: Context) : Interceptor {
+
+    private val netLogDb = Room.databaseBuilder(context.applicationContext, OkLogNetDb::class.java,
+            "oknetlog.db")
+            .build()
+
+    private val okNetLogDao = netLogDb.netLogDao
 
     override fun intercept(chain: Chain): Response {
         return chain.proceed(chain.request())
