1353255c124d2a016bead474551a44ffe96b8e97
Vinay Shenoy <vinaysshenoy@gmail.com>
2018-01-16 15:09:36 +0530

    Add lib

--- app/src/main/java/com/vinaysshenoy/oklognet/app/MainActivity.kt
+++ app/src/main/java/com/vinaysshenoy/oklognet/app/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.vinaysshenoy.oklognet
+package com.vinaysshenoy.oklognet.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.vinaysshenoy.oklognet.app.R.layout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
     }
 }
