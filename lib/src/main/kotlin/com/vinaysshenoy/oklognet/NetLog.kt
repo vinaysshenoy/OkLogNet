d283783c261ff60c258b74bbd3ba582c35e44373
Vinay Shenoy <vinaysshenoy@gmail.com>
2018-01-16 16:20:49 +0530

    Added Entity class for Net Logs

--- lib/src/main/kotlin/com/vinaysshenoy/oklognet/NetLog.kt
+++ lib/src/main/kotlin/com/vinaysshenoy/oklognet/NetLog.kt
@@ -1,10 +1,12 @@
 package com.vinaysshenoy.oklognet
 
+import java.util.Date
+
 /**
  * Created by vinaysshenoy on 16/01/18.
  */
 data class NetLog(
-        val timeStamp: String,
+        val requestTime: Date,
         val url: String,
         val method: String,
         val requestHeaders: Map<String, String>,
---
+++ lib/src/main/kotlin/com/vinaysshenoy/oklognet/db/NetLogEntity.kt
@@ -0,0 +1,43 @@
+package com.vinaysshenoy.oklognet.db
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ * Created by vinaysshenoy on 16/01/18.
+ */
+@Entity
+internal class NetLogEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id = 0L
+
+    @ColumnInfo(name = "url")
+    var url = ""
+
+    @ColumnInfo(name = "request_time")
+    var requestTime = 0L
+
+    @ColumnInfo(name = "method")
+    var method = ""
+
+    @ColumnInfo(name = "request_headers")
+    var requestHeaders = ""
+
+    @ColumnInfo(name = "request_body")
+    var requestBody = ""
+
+    @ColumnInfo(name = "response_code")
+    var responseCode = 0
+
+    @ColumnInfo(name = "response_headers")
+    var responseHeaders = ""
+
+    @ColumnInfo(name = "response_body")
+    var responseBody = ""
+
+    @ColumnInfo(name = "duration")
+    var duration = 0L
+}
