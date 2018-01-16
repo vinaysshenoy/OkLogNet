9e89be82a44f445ece67bc412320a7cf3f8a61c7
Vinay Shenoy <vinaysshenoy@gmail.com>
2018-01-16 17:13:41 +0530

    Added method to retrieve all net logs from OkLogNet

--- lib/src/main/kotlin/com/vinaysshenoy/oklognet/OkLogNet.kt
+++ lib/src/main/kotlin/com/vinaysshenoy/oklognet/OkLogNet.kt
@@ -2,22 +2,47 @@ package com.vinaysshenoy.oklognet
 
 import android.arch.persistence.room.Room
 import android.content.Context
+import com.vinaysshenoy.oklognet.db.NetLogEntity
 import com.vinaysshenoy.oklognet.db.OkLogNetDb
 import okhttp3.Interceptor
+import java.util.Date
 
 /**
  * Created by vinaysshenoy on 16/01/18.
  */
 class OkLogNet(context: Context) {
 
-    private val netLogDb by lazy {
+    private val okLogNetDb by lazy {
         Room.databaseBuilder(context.applicationContext, OkLogNetDb::class.java,
                 "oknetlog.db")
                 .build()
     }
 
-    private val okNetLogDao by lazy { netLogDb.netLogDao }
+    private val netLogDao by lazy { okLogNetDb.netLogDao }
 
-    val interceptor by lazy { OkLogNetInterceptor(okNetLogDao) as Interceptor }
+    val interceptor by lazy { OkLogNetInterceptor(netLogDao) as Interceptor }
+
+    fun getAllNetLogs() = netLogDao.getAll().map(this::mapNetLogEntitityToModel)
+
+    private fun mapNetLogEntitityToModel(entity: NetLogEntity) =
+            NetLog(
+                    requestTime = Date(entity.requestTime),
+                    url = entity.url,
+                    method = entity.method,
+                    requestHeaders = convertHeaderStringToHeaderMap(entity.requestHeaders),
+                    requestBody = entity.requestBody,
+                    responseCode = entity.responseCode,
+                    responseHeaders = convertHeaderStringToHeaderMap(entity.responseHeaders),
+                    responseBody = entity.responseBody,
+                    duration = entity.duration)
+
+    //TODO: Test this chain
+    private fun convertHeaderStringToHeaderMap(headerString: String)
+            = headerString
+            .split("|")
+            .filter { it.contains("=") }
+            .map { it.split("=") }
+            .filter { it.size == 2 }
+            .associate { Pair(it[0], it[1]) }
 
 }
